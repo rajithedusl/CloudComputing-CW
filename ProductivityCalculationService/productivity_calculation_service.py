62102272a3a69c92492f1779054e39367572c00f
rajithedusl <163971053+rajithedusl@users.noreply.github.com>
2024-03-27 14:09:29 +0530

    Update productivity_calculation_service.py

--- ProductivityCalculationService/productivity_calculation_service.py
+++ ProductivityCalculationService/productivity_calculation_service.py
@@ -1,6 +1,7 @@
 """
 This service would perform the calculation for the productivity metric for every developer and rank the developers
 based on the score and would return the results to the client
+test
 """
 import json
 import logging
